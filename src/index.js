06bec528c8035ff79861ba7a055ffe14682120a4
miho yamagata <miho.yamagata@gmail.com>
2020-03-22 17:22:55 +0900

    edit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import _ from 'lodash';
 
+let counter = 0;
+
 function addRandomNum() {
-  const mainEl = document.getElementById('main');
-  let p = document.createElement('p')
-  p.innerHTML = "Random Number: " + _.random(0, 10).toString();
-  document.body.appendChild(p)
+  if (counter < 5) {
+    const mainEl = document.getElementById('main');
+    let p = document.createElement('p')
+    p.innerHTML = "Random Number: " + _.random(0, 10).toString();
+    document.body.appendChild(p);
+    counter++;
+  }
 }
 
-{
-  const button1 = document.getElementById('button1')
+function recordNumber() {
+  const button1 = document.getElementById('button1');
   button1.addEventListener("click", addRandomNum);
   console.log("ready");
 }
+
+recordNumber();
+
+
+
+
